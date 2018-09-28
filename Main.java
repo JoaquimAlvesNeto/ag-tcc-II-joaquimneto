7a62fd22805e730910bb021027bdc567c8b6de7b
JoaquimAlvesNeto <joaquimalves1994@gmail.com>
2018-09-28 12:10:10 -0300

    AG-Projeto
    Segue os arquivos.

--- Main.java
+++ Main.java
@@ -1,20 +1,15 @@
-
-package classes;
-
 public class Main {
-    
-    public Main() {
-        
-    }
-    
-    public static void main(String[] args) {
-        
-        Cromossomo c = new Cromossomo();
-        c.preencher();
-        c.mostrar();
-        c.getPerfis();
-        Funcao_avaliacao f = new Funcao_avaliacao(c);
-        f.calculo_distribuicao_turma_A();
-    }
-    
+	public static void main(String args[]) {
+	   
+            final int num_geracoes = 10;
+            final int tam_populacao = 100;
+            final double prob_mutacao = 0.01;
+            
+           //GA meuGA=new GA(num_geracoes,tam_populacao,prob_mutacao);
+           // meuGA.executa();
+            ElementoGA pai = new ElementoGA();
+            pai.inicializaElemento(100);
+            pai.contagemCaracteres();
+            System.out.println("RESULTADO FINAL: " + pai.calculo_distribuicao_turma_A());
+	}
 }
